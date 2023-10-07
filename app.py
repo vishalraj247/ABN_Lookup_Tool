5d5441092b3a18eb7cff091a8a00829b46880a9d
Vishal Raj <vishalraj247@gmail.com>
2023-10-08 05:05:23 +1100

    Update App

--- app.py
+++ app.py
@@ -2,17 +2,23 @@ import streamlit as st
 import pandas as pd
 import os
 
-# Assuming you have these modules and functions
 from keywordgen import generate_business_name_suggestions
 from abnlookup import query_postcodes
 
 def reset_session_state():
-    st.session_state.suggested_names = None
+    st.session_state.initial_suggestions = None
+    st.session_state.refined_names = None
+    st.session_state.combined_suggestions = None
     st.session_state.result_df = None
 
-# Set default values in the session state if they don't exist
-if 'suggested_names' not in st.session_state:
-    st.session_state.suggested_names = None
+if 'initial_suggestions' not in st.session_state:
+    st.session_state.initial_suggestions = None
+
+if 'refined_names' not in st.session_state:
+    st.session_state.refined_names = None
+
+if 'combined_suggestions' not in st.session_state:
+    st.session_state.combined_suggestions = None
 
 if 'result_df' not in st.session_state:
     st.session_state.result_df = None
@@ -23,44 +29,52 @@ if 'industry_input' not in st.session_state:
 if 'postcode_input' not in st.session_state:
     st.session_state.postcode_input = ""
 
-# Set page config
 st.set_page_config(page_title="ABN Lookup Tool", page_icon="🔍", layout='wide', initial_sidebar_state="expanded")
 
-# Title of the app
 st.title("ABN Lookup Tool")
 
-# Add a sidebar
 sidebar = st.sidebar
 sidebar.title("Navigation")
 page = sidebar.radio("Select a Page", ["Home", "Contact Us", "Feedback"])
 
-# Content of pages
 if page == "Home":
     st.sidebar.header("User Input Features")
-    user_industry_input = st.sidebar.text_input("Enter Industry:", value=st.session_state.industry_input)
-    user_postcode_input = st.sidebar.text_input("Enter Postcode:", value=st.session_state.postcode_input, max_chars=4)
+    st.session_state.industry_input = st.sidebar.text_input("Enter Industry:", value=st.session_state.industry_input)
+    st.session_state.postcode_input = st.sidebar.text_input("Enter Postcode:", value=st.session_state.postcode_input, max_chars=4)
 
     if st.sidebar.button("Generate Suggestions"):
-        if user_industry_input and user_postcode_input:
-            reset_session_state()
-            st.session_state.industry_input = user_industry_input
-            st.session_state.postcode_input = user_postcode_input
-            
-            with st.spinner('Generating Business Names...'):
-                st.session_state.suggested_names = generate_business_name_suggestions(st.session_state.industry_input)
-                while not st.session_state.suggested_names:
-                    st.warning("No suggestions generated. Regenerating...")
-                    st.session_state.suggested_names = generate_business_name_suggestions(st.session_state.industry_input)
+        reset_session_state()
+        additional_names = ["Bugz Off", "Sydney Side Pest", "Pest Control", "Pesticides"]  # Example additional names
+        # Including a spinner here for user feedback during processing
+        with st.spinner('Generating Business Names...'):
+            initial_suggestions, refined_names, combined_suggestions = generate_business_name_suggestions(st.session_state.industry_input, additional_names)
+            # Adding a warning and regeneration loop in case no suggestions are returned
+            while not initial_suggestions:
+                st.warning("No initial suggestions generated. Regenerating...")
+                initial_suggestions, refined_names, combined_suggestions = generate_business_name_suggestions(st.session_state.industry_input, additional_names)
+            st.session_state.initial_suggestions = initial_suggestions
+            st.session_state.refined_names = refined_names
+            st.session_state.combined_suggestions = combined_suggestions
 
-    if st.session_state.suggested_names:
-        st.subheader(f"Suggested Names for {st.session_state.industry_input} Industry:")
-        edited_suggested_names = st.text_area("Edit Suggested Names:", ', '.join(st.session_state.suggested_names))
-        st.session_state.suggested_names = edited_suggested_names.split(', ')
+    if st.session_state.initial_suggestions:
+        st.subheader(f"Initial suggested Names for {st.session_state.industry_input} Industry:")
+        edited_initial_suggestions = st.text_area("Edit Initial Suggested Names:", ', '.join(st.session_state.initial_suggestions))
+        st.session_state.initial_suggestions = edited_initial_suggestions.split(', ')
+
+    if st.session_state.refined_names:
+        st.subheader(f"Refined Names for {st.session_state.industry_input} Industry:")
+        edited_refined_names = st.text_area("Edit Refined Names:", ', '.join(st.session_state.refined_names))
+        st.session_state.refined_names = edited_refined_names.split(', ')
+
+    if st.session_state.combined_suggestions:
+        st.subheader(f"Combined Suggestions for {st.session_state.industry_input} Industry:")
+        edited_combined_suggestions = st.text_area("Edit Combined Suggestions:", ', '.join(st.session_state.combined_suggestions))
+        st.session_state.combined_suggestions = edited_combined_suggestions.split(', ')
 
         if st.button("Explore Businesses Now"):
             with st.spinner('Fetching Business Information...'):
                 postcodes_to_query = {st.session_state.postcode_input}
-                st.session_state.result_df = query_postcodes(postcodes_to_query, st.session_state.suggested_names)
+                st.session_state.result_df = query_postcodes(postcodes_to_query, st.session_state.combined_suggestions)
 
     if st.session_state.result_df is not None and not st.session_state.result_df.empty:
         st.subheader(f"Businesses in {st.session_state.postcode_input} related to {st.session_state.industry_input}:")
--- keywordgen.py
+++ keywordgen.py
@@ -1,3 +1,4 @@
+'''
 import os
 import openai
 import re
@@ -27,3 +28,76 @@ def generate_business_name_suggestions(industry):
     suggestions = [word.capitalize() for word in suggestions if word.isalpha()]
 
     return suggestions
+'''
+
+import os
+import openai
+import re
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+def generate_business_name_suggestions(industry, additional_names):
+    # Define a prompt to generate initial suggestions
+    initial_prompt = f"Please generate a list of unique, one-word keywords that are strictly and directly related to the {industry} industry. These keywords should be commonly associated with businesses in the {industry} sector and should not include generic words or phrases."
+
+    # Query GPT with the initial prompt
+    initial_response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": initial_prompt}],
+        max_tokens=100,
+        temperature=0.7
+    )
+
+    # Extract and parse the initial suggestions from GPT's response
+    initial_suggestions_text = initial_response['choices'][0]['message']['content']
+    initial_suggestions = re.findall(r'\b[A-Z][a-z]*\b', initial_suggestions_text)
+    initial_suggestions = [word.capitalize() for word in initial_suggestions if word.isalpha()]
+    print(initial_suggestions)
+    # Refine business names
+    additional_names = ["Bugz Off", "Sydney Side Pest", "Pest Control", "Pesticides"]
+    refine_prompt = f"The following is a list of business names related to the {industry} industry: {', '.join(additional_names)}. You can keep the parts of business names that might sound related but might not exactly correspond to typical keywords in the {industry} industry. For example, for 'Sydney Side Pest' and 'Bugz Off' in the Pest Control Industry, keep 'Pest' and 'Bugz' but remove 'Sydney', 'Side' and 'Off'. Please carefully review the list. For each word, if it isn't a word a little related to the {industry} industry, kindly remove it, even if it forms a part of a business name. Leaving behind a list of words that are uniquely related to the {industry} industry."
+    refine_response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": refine_prompt}],
+        max_tokens=500,
+        temperature=0.7
+    )
+    refined_names_text = refine_response['choices'][0]['message']['content']
+    refined_names = re.findall(r'\b[A-Z][a-z]*\b', refined_names_text)
+    refined_names = [word.capitalize() for word in refined_names if word.isalpha()]
+    #Testing
+    print(refined_names)
+    # Step 3: Combine and deduplicate
+    combined_suggestions = list(set(initial_suggestions + refined_names))
+
+    return initial_suggestions, refined_names, combined_suggestions
+'''
+    # Testing
+    print(initial_suggestions)
+    # Combine the initial suggestions with the additional names
+    combined_suggestions = list(set(initial_suggestions + additional_names))
+    print(combined_suggestions)
+    # Define a second prompt to verify the relevance of the combined suggestions
+    verification_prompt = f"""
+    The following is a list of words, each word being either a keyword or a business name related to the {industry} industry: {', '.join(combined_suggestions)}. 
+    The keywords are strictly related to the {industry} industry. However, you can keep the parts of business names which might sound related but might not 
+    exactly correspond to typical keywords in the {industry} industry. For example, for 'Sydney Side Pest' and 'Bugz Off' in Pest Control Industry, keep 'Pest' and 'Bugz' but remove 'Sydney', 'Side' and 'Off'. 
+    Please carefully review the list. For each word, if it isn't a word strictly related to the {industry} industry, kindly remove it, even if it forms part of a business name. 
+    Additionally, please eliminate any duplicates, leaving behind a list of words that are uniquely and directly related to the {industry} industry.
+    """
+
+    # Query GPT with the verification prompt
+    verification_response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": verification_prompt}],
+        max_tokens=500,
+        temperature=0.7
+    )
+
+    # Extract, parse, and return the final suggestions from GPT's response
+    final_suggestions_text = verification_response['choices'][0]['message']['content']
+    final_suggestions = re.findall(r'\b[A-Z][a-z]*\b', final_suggestions_text)
+    final_suggestions = [word.capitalize() for word in final_suggestions if word.isalpha()]
+
+    return final_suggestions
+'''
